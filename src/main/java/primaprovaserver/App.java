9d36e28ab84e8918f2cddc294c6a62b88363fe56
Ostrica <matteottria@gmail.com>
2022-10-09 22:43:09 +0200

    Test for a smart sever

--- src/main/java/primaprovaserver/App.java
+++ src/main/java/primaprovaserver/App.java
@@ -15,16 +15,37 @@ public class App
                     System.out.println("Client connected.");
                     InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
                     BufferedReader bufferReader = new BufferedReader(streamReader);
-                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-                    printWriter.println("You are connect to the server."); 
-                    printWriter.flush();
+                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+                    printWriter.println("You are connect to the server.");
                     String str;
-                    str = bufferReader.readLine();
-                    System.out.println(str);
+                    if ((str = bufferReader.readLine()) != null){
+                        printWriter.println(str);
+                        gestore(str, printWriter);
+                    }
                 }
             }
         } catch (Exception e) {
             System.out.println(e);
+        }
+    }
+
+    public static void gestore(String str, PrintWriter printWriter) throws Exception{
+        double altezza = 0;
+        double peso = 0;
+        double bmi = 0;
+        while (true) {
+            String[] words = str.split("\\W+");
+            if(words[1] == "altezza"){
+                altezza = Double.parseDouble(words[2]);
+                printWriter.println(altezza);
+            }else if (words[1] == "peso") {
+                peso = Double.parseDouble(words[2]);
+                printWriter.println(peso);
+            }else if (words[1] == "calcola"){
+                bmi = peso/(altezza * altezza);
+                printWriter.println(bmi);
+            }
+            return;
         }
     }
 }
