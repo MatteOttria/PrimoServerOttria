3b115eb4f3dc29cdf0d3ced2abb8547449034864
Ostrica <matteottria@gmail.com>
2022-10-11 19:09:08 +0200

    something is different

--- src/main/java/primaprovaserver/App.java
+++ src/main/java/primaprovaserver/App.java
@@ -8,44 +8,44 @@ public class App
 {
     public static void main(String [] args) throws Exception
     {
-        try(ServerSocket serverSocket = new ServerSocket(5001)){
+        try(ServerSocket serverSocket = new ServerSocket(1723)){
             System.out.println("Server opened.");
             while(true){
-                try(Socket socket = serverSocket.accept()){
+                    Socket socket = serverSocket.accept();
                     System.out.println("Client connected.");
                     InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
                     BufferedReader bufferReader = new BufferedReader(streamReader);
                     PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
                     printWriter.println("You are connect to the server.");
-                    String str;
-                    if ((str = bufferReader.readLine()) != null){
-                        printWriter.println(str);
-                        gestore(str, printWriter);
-                    }
-                }
-            }
+                    gestore(printWriter, bufferReader);
+                }   
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public static void gestore(String str, PrintWriter printWriter) throws Exception{
+    public static void gestore(PrintWriter printWriter, BufferedReader bufferedReader) throws Exception{
         double altezza = 0;
         double peso = 0;
         double bmi = 0;
-        while (true) {
-            String[] words = str.split("\\W+");
-            if(words[1] == "altezza"){
-                altezza = Double.parseDouble(words[2]);
-                printWriter.println(altezza);
-            }else if (words[1] == "peso") {
-                peso = Double.parseDouble(words[2]);
-                printWriter.println(peso);
-            }else if (words[1] == "calcola"){
+        while (bmi == 0) {
+            String str = bufferedReader.readLine();
+            String[] words = str.split(" ");
+            if(words[0].equals("altezza")){
+                altezza = Double.parseDouble(words[1]);
+                printWriter.println("altezza inserita");
+            }else if (words[0].equals("peso")) {
+                peso = Double.parseDouble(words[1]);
+                printWriter.println("peso inserito");
+            }else if (words[0].equals("calcola")){
                 bmi = peso/(altezza * altezza);
-                printWriter.println(bmi);
+                printWriter.println("calcolo eseguito:" + bmi);
+            }else if(words[0].equals("stop")){
+                printWriter.println("server chiuso");
+                return;
             }
-            return;
         }
+        System.out.println(bmi);
+        return;
     }
 }
