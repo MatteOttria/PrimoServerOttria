d87b6db8712bb5f733a2aa0809db3cfdccfe770d
Ostrica <78510678+MatteOttria@users.noreply.github.com>
2022-10-12 09:03:12 +0200

    Something is different

--- src/main/java/primaprovaserver/App.java
+++ src/main/java/primaprovaserver/App.java
@@ -16,7 +16,7 @@ public class App
                     InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
                     BufferedReader bufferReader = new BufferedReader(streamReader);
                     PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-                    printWriter.println("You are connect to the server.");
+                    printWriter.println("You are connect to the server. Comandi possibili: 1)calcola");
                     gestore(printWriter, bufferReader);
                 }   
         } catch (Exception e) {
@@ -24,28 +24,42 @@ public class App
         }
     }
 
-    public static void gestore(PrintWriter printWriter, BufferedReader bufferedReader) throws Exception{
+    public static void gestore(PrintWriter printWriter, BufferedReader bufferedReader)throws Exception{
+        while (true) {
+            String str = bufferedReader.readLine();
+            if (str.equals("calcola")) {
+                massaCorporea(printWriter,bufferedReader);
+            }else{
+                printWriter.println("Comando: " + str + " non riconosciuto.");
+            }
+        }
+    }
+
+    public static void massaCorporea(PrintWriter printWriter, BufferedReader bufferedReader) throws Exception{
         double altezza = 0;
         double peso = 0;
         double bmi = 0;
+        printWriter.println("Comandi possibili: 1)altezza 'var', 2)peso 'var', 3)calcola, 4)stop");
         while (bmi == 0) {
             String str = bufferedReader.readLine();
             String[] words = str.split(" ");
             if(words[0].equals("altezza")){
                 altezza = Double.parseDouble(words[1]);
-                printWriter.println("altezza inserita");
+                printWriter.println("Altezza inserita");
             }else if (words[0].equals("peso")) {
                 peso = Double.parseDouble(words[1]);
-                printWriter.println("peso inserito");
+                printWriter.println("Peso inserito");
             }else if (words[0].equals("calcola")){
                 bmi = peso/(altezza * altezza);
-                printWriter.println("calcolo eseguito:" + bmi);
+                bmi = Math.round(bmi * 1000.0)/1000.0;
+                printWriter.println("Calcolo eseguito:" + bmi);
             }else if(words[0].equals("stop")){
-                printWriter.println("server chiuso");
                 return;
+            }else{
+                printWriter.println("Comando: " + str + "non riconosciuto.");
             }
         }
-        System.out.println(bmi);
-        return;
+            System.out.println(bmi);
+            return;
     }
 }
